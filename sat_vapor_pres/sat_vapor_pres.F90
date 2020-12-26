c6ba098f83b84bd5e1461bc6dcc9827426692d2d
valery.yudin <valery.yudin@noaa.gov>
2020-12-26 00:21:08 -0500

    integer, public :: tcmax =  160

--- sat_vapor_pres/sat_vapor_pres.F90
+++ sat_vapor_pres/sat_vapor_pres.F90
@@ -523,7 +523,7 @@ private
 !  parameters for table size and resolution
 
  integer, public :: tcmin = -160  ! minimum temperature (degC) in lookup table
- integer, public :: tcmax =  100  ! maximum temperature (degC) in lookup table
+ integer, public :: tcmax =  160  ! maximum temperature (degC) in lookup table
  integer :: esres =  10   ! table resolution (increments per degree)
  integer :: nsize  ! (tcmax-tcmin)*esres+1    !  lookup table size
  integer :: nlim   ! nsize-1
